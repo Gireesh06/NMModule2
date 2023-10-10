7ce0f6312a213e5e68e78b22a0617060df60c5f1
Gireesh06 <147477362+Gireesh06@users.noreply.github.com>
2023-10-10 12:07:50 +0530

    Add files via upload

---
+++ foreach.js
@@ -0,0 +1,9 @@
+var friend = ["aaa","bbb","ccc","ddd"];
+friend.forEach( (item,index) =>{
+    console.log(item,index);
+} );
+
+var fname ="gireesh";
+fname.forEach((letter,index) => {
+    console.index(letter,index);
+});
